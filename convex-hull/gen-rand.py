5eebd523d78d3722cf81e2b592a012b8bfac98dd
Sarvottamananda <ss@cix.lan>
2025-08-16 03:34:52 +0530

    modified meson.build

---
+++ convex-hull/gen-rand.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import random
+
+for i in range (1,30) :
+    while (True) :
+        x = random.random() * 9.9 - 5
+        y = random.random() * 9.9 - 5
+        if (x*x + y*y < 25) :
+            break
+    print ('%3.1f ' % (x + 5) , '%3.1f' % (y + 5))
+
