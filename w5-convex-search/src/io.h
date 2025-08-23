3ec3f9b403791ce83c5d3320ac453fbbd802ef65
sarvottamananda <sarvottamananda@gmail.com>
2025-08-23 14:07:06 +0530

    Deleted solution code

--- w5-convex-search/src/io.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-void read_input(char *, char *, PointList &, PointList &);
-
-void print_list(const char *, const PointList &);
-void print_point(const char *, const Point &);
--- w5-convex-search/src/search.h
+++
@@ -1,7 +0,0 @@
-#ifndef SEARCH_H_INCD
-#define SEARCH_H_INCD
-
-void pre_process(const PointList&, PointList&, PointList&);
-void search(const PointList&, const PointList&, const PointList&);
-
-#endif
